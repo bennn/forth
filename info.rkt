6548afd6d4bb0fff97f2df472e83d67a2a2dde9f
Ben Greenman <types@ccs.neu.edu>
2015-11-10 02:12:02 -0500

    ** packaging, travis

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang info
+
+(define collection "forth")
+(define deps '("base"))
+(define build-deps '(
+  "cover"
+  "cover-coveralls"
+  "scribble-lib"
+  "racket-doc"
+  "rackunit-lib"
+  "rackunit-abbrevs"))
+(define pkg-desc "Forth emulator")
+(define version "0.0")
+(define pkg-authors '(ben))
+(define scribblings '(("scribblings/forth.scrbl")))
+(define raco-commands '(("forth" (submod forth main) "Open a Forth REPL session" #f)))

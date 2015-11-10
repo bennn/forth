79060845fb94cfa0fd7401842e06fb16eb2fe86d
Ben Greenman <types@ccs.neu.edu>
2015-11-10 17:44:36 -0500

    ** removed xrepl

--- main.rkt
+++ main.rkt
@@ -1,7 +1,5 @@
 #lang racket/base
 
 (module+ main
-  ;; Maybe, move this to a main.rkt file
-  (require xrepl forth/private/stack forth/private/command)
-  (forth-repl)
-) 
+  (require forth/private/stack forth/private/command)
+  (forth-repl))
